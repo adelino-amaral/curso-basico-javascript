02c6ea8a888e8989449a8169b43ac37c5a494dbb
Adelino Amaral <adelino.amaral@gmail.com>
2019-08-07 17:23:22 +0100

    Tirei exercicios

--- BASE/4-functions/EXEMPLOS/Conversoes/celsius_to.js
+++
@@ -1,9 +0,0 @@
-function Celsius_Fahrenheit_Converter(valNum) {
-  valNum = parseFloat(valNum);
-  document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
-}
-
-function Celsius_Kelvin_Converter(valNum) {
-  valNum = parseFloat(valNum);
-  document.getElementById("outputKelvin").innerHTML=valNum+273.15;
-}
